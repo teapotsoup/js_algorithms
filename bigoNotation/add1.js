44afba53aa1159a5f7419c20aa4c16a5f896e0f9
teapotsoup <0902ab@naver.com>
2022-10-23 12:06:23 +0900

    second

--- bigoNotation/add1.js
+++ bigoNotation/add1.js
@@ -1,4 +1,4 @@
-function addUpTo(n) {
+function addUpTo1(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
     total += i;
@@ -6,4 +6,17 @@ function addUpTo(n) {
   return total;
 }
 
-console.log(addUpTo(6));
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+let t1 = performance.now();
+addUpTo1(1000000);
+let t2 = performance.now();
+
+let t3 = performance.now();
+addUpTo2(1000000);
+let t4 = performance.now();
+
+console.log(`${(t2 - t1) / 1000} seconds`);
+console.log(`${(t4 - t3) / 1000} seconds`);
