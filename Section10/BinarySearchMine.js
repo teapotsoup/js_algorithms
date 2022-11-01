47496ea575f7dac91b0d949c87d0e9f66e51f479
teapotsoup <0902ab@naver.com>
2022-11-02 00:06:13 +0900

    fixed binary

--- Section10/BinarySearchMine.js
+++ Section10/BinarySearchMine.js
@@ -1,23 +1,14 @@
-// 좌측 포인터가 우측 포인터보다 앞에 있는 동안만
-// 값이 작으면 스타트포인터 중간 인덱스
-// 값이 크면 엔드 포인터 중간 인덱스
-// 반복 끝나도 일치 없으면 -1
-
 function BinarySearch(arr, num) {
   let start = 0;
   let end = arr.length - 1;
 
-  while (start < end - 1) {
-    // 0 19
-    let temp = parseInt((start + end) / 2); //  9
-    if (num === arr[temp]) {
+  while (start < end) {
+    if (start + 1 === end) {
+      return -1;
+    }
+    let temp = parseInt((start + end) / 2);
+    if (arr[temp] === num) {
       return temp;
-    }
-    if (num < arr[temp]) {
-      end = temp;
-    }
-    if (num > arr[temp]) {
-      start = temp;
     }
     if (arr[end] === num) {
       return end;
@@ -25,12 +16,17 @@ function BinarySearch(arr, num) {
     if (arr[start] === num) {
       return start;
     }
+    if (num < arr[temp]) {
+      end = temp;
+    }
+    if (num > arr[temp]) {
+      start = temp;
+    }
   }
   return -1;
 }
 
-// console.log(BinarySearch([5, 6, 10, 13, 14, 18, 30, 34], 10)); // 2
-
+console.log(BinarySearch([5, 6, 10, 13, 14, 18, 30, 34], 20)); // 5
 // console.log(BinarySearch([1, 2, 3, 4, 5], 3)); // 2
 // console.log(BinarySearch([1, 2, 3, 4, 5], 5)); // 4
 // console.log(BinarySearch([1, 2, 3, 4, 5], 6)); // -1
@@ -54,7 +50,7 @@ console.log(
     95
   )
 );
-16;
+//16;
 
 console.log(
   BinarySearch(
